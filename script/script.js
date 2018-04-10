2fe2c6d51eb28cb74941fe4a9dadec0e2c4e17b6
raghav karkera <karkeraraghav@gmail.com>
2018-04-10 18:08:05 -0400

    after changes

--- script/script.js
+++ script/script.js
@@ -6,9 +6,11 @@ Survey.Survey.cssType = "bootstrap";
 function printResult(results){
     var results = results;
     
-    var printChart = getChartData(results);
+    var i = getChartData(results)
+    
      //console.log(printChart);
-    makechart(printChart);
+    makechart(i.data,i.dataO,i.dataC,i.dataE,i.dataA,i.dataN);
+   
     document.getElementById("ocean").style.display = "block";
  
     console.log(results);
@@ -16,9 +18,9 @@ function printResult(results){
     
 };
 
-    var survey = new Survey.Model(surveyJSON);
-    $("#surveyElement").Survey({model:survey});
-    survey.onComplete.add(function (result) {
+var survey = new Survey.Model(surveyJSON);
+$("#surveyElement").Survey({model:survey});
+survey.onComplete.add(function (result) {
         printResult(result.data);
 //        document.querySelector('#surveyResult').innerHTML = "From the way you answered the questions, you seem to describe yourself as someone who is energetic and active. Your answers describe you as someone who has the following traits";
         document.getElementById("surveyResult").style.display = "block";
@@ -27,9 +29,10 @@ function printResult(results){
 
 
 //make the chart, pass result data
-    makechart = function(printChart){
-    var ctx = document.getElementById("myChart");
+makechart = function(printChart,dataO){
     
+    //print volume chart
+    var ctx = document.getElementById("myChart");
     //remove the scale    
     var options = {scale: {display: false }};
     var myChart = new Chart(ctx, {
@@ -37,16 +40,100 @@ function printResult(results){
         data: printChart,
         options: options
     });
+    
+    var opennessChart = document.getElementById("chartO");
+    var myDoughnutChart = new Chart(opennessChart, {
+        type: 'doughnut',
+        data: dataO,
+        options: {
+             legend: {display: false},
+             tooltips: {enabled: false}
+        }
+    });
+    
+    var opennessChart = document.getElementById("chartC");
+    var myDoughnutChart = new Chart(opennessChart, {
+        type: 'doughnut',
+        data: dataC,
+        options: {
+             legend: {display: false},
+             tooltips: {enabled: false}
+        }
+    });
+    
+    var opennessChart = document.getElementById("chartE");
+    var myDoughnutChart = new Chart(opennessChart, {
+        type: 'doughnut',
+        data: dataE,
+        options: {
+             legend: {display: false},
+             tooltips: {enabled: false}
+        }
+    });
+    
+    var opennessChart = document.getElementById("chartA");
+    var myDoughnutChart = new Chart(opennessChart, {
+        type: 'doughnut',
+        data: dataA,
+        options: {
+             legend: {display: false},
+             tooltips: {enabled: false}
+        }
+    });
+    
+    var opennessChart = document.getElementById("chartN");
+    var myDoughnutChart = new Chart(opennessChart, {
+        type: 'doughnut',
+        data: dataN,
+        options: {
+             legend: {display: false},
+             tooltips: {enabled: false}
+        }
+    });
+
 };
 
 //parse json object to create chart data
-    getChartData = function(result){
+getChartData = function(result){
     
-    total_score = result.openness + result.conscientiousness + result.extraversion + result.agreeableness + result.neuroticism + result.openness_r + result.conscientiousness_r + result.extraversion_r + result.agreeableness_r + result.neuroticism_r;
+    
+    openness = result.openness+result.openness_r;
+    conscientiousness = result.conscientiousness+result.conscientiousness_r;
+    extraversion = result.extraversion+result.extraversion_r;
+    agreeableness = result.agreeableness+result.agreeableness_r;
+    neuroticism = result.neuroticism+result.neuroticism_r;
+    
+    //O
+    if(openness >= 7)
+        document.getElementById("Creative").style.display = "block";
+    else 
+        document.getElementById("Conventional").style.display = "block";
+   //C
+    if(conscientiousness >= 7)
+        document.getElementById("Dependable").style.display = "block";
+    else 
+        document.getElementById("Careless").style.display = "block";
+    
+    //E
+    if(extraversion >= 7)
+        document.getElementById("Extroverted").style.display = "block";
+    else 
+        document.getElementById("Reserved").style.display = "block";
+    //A
+    if(agreeableness >= 7)
+        document.getElementById("Warm").style.display = "block";
+    else 
+        document.getElementById("Critical").style.display = "block";
+    //N
+    if(neuroticism >= 7)
+        document.getElementById("Anxious").style.display = "block";
+    else 
+        document.getElementById("Calm").style.display = "block";
+    
     data = {
     datasets: [{
 //        data: [ Math.round((result.openness+result.openness_r)/ total_score *100), Math.round((result.conscientiousness+result.conscientiousness_r)/ total_score *100), Math.round((result.extraversion+result.extraversion_r)/ total_score *100),Math.round((result.agreeableness+result.agreeableness_r)/ total_score *100), Math.round((result.neuroticism+result.neuroticism_r)/ total_score *100)],
-        data: [ result.openness+result.openness_r, result.conscientiousness+result.conscientiousness_r, result.extraversion+result.extraversion_r,result.agreeableness+result.agreeableness_r, result.neuroticism+result.neuroticism_r],
+        data: [ openness, conscientiousness, extraversion,agreeableness, neuroticism],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -66,6 +153,45 @@ function printResult(results){
     // These labels appear in the legend and in the tooltips when hovering different arcs
     labels: ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Neuroticism"],
     };
-    return data;
+    
+    dataO = {
+        datasets: [{
+            data: [openness, 14],
+            backgroundColor: ["#0074D9", "#FF4136"],
+        }]
+    };
+     dataC = {
+        datasets: [{
+            data: [conscientiousness, 14],
+            backgroundColor: ["#0074D9", "#FF4136"],
+        }]
+    };
+     dataE = {
+        datasets: [{
+            data: [extraversion, 14],
+            backgroundColor: ["#0074D9", "#FF4136"],
+        }]
+    };
+     dataA = {
+        datasets: [{
+            data: [agreeableness, 14],
+            backgroundColor: ["#0074D9", "#FF4136"],
+        }]
+    };
+     dataN = {
+        datasets: [{
+            data: [neuroticism, 14],
+            backgroundColor: ["#0074D9", "#FF4136"],
+        }]
+    };
+    
+    return{
+        data: data,
+        dataO: dataO,
+        dataC: dataC,
+        dataE : dataE,
+        dataA : dataA,
+        dataN : dataN,
+    };
     
 }
